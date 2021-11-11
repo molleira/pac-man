00a2dd99cc184ee4344d65787aa1b9899cebc034
Marc Oller <marc.om@icloud.com>
2021-11-11 16:41:50 +0100

    Create ghost class

--- app.js
+++ app.js
@@ -142,3 +142,19 @@ function eatDot() {
     scoreDisplay.innerHTML = score
   }
 }
+
+// define ghosts
+class Ghost {
+  constructor(className, startIndex, speed) {
+    this.className = className
+    this.startIndex = startIndex
+    this.speed = speed
+  }
+}
+
+ghosts = [
+  new Ghost('blinky', 348, 250),
+  new Ghost('pinky', 376, 400),
+  new Ghost('inky', 351, 300),
+  new Ghost('clyde', 379, 500)
+]
